ef5be760925d6224e3fba30b221e89574d09e6d2
Yann Malet <yann.malet@gmail.com>
2016-10-06 16:06:19 +0200

    Lower case the metric name and replace `.` by `-`

--- metricbeat/module/uwsgi/stats/data.go
+++ metricbeat/module/uwsgi/stats/data.go
@@ -3,17 +3,17 @@ package stats
 import (
 	"fmt"
 	"reflect"
+	"strings"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/metricbeat/module/uwsgi"
 )
 
 func getKey(name, prefix string) string {
 	if prefix != "" {
-		name = fmt.Sprintf("%s.%s", prefix, name)
+		name = fmt.Sprintf("%s-%s", prefix, name)
 	}
-	return name
+	return strings.ToLower(name)
 
 }
 
@@ -29,16 +29,12 @@ func getFlatMapStr(val reflect.Value, prefix string) common.MapStr {
 			m[getKey(typeOfT.Field(i).Name, prefix)] = val.Field(i).String()
 		case reflect.Slice:
 			for j := 0; j < val.Field(i).Len(); j++ {
-				m = common.MapStrUnion(m, getFlatMapStr(val.Field(i).Index(j), fmt.Sprintf("%s.%d", typeOfT.Field(j).Name, j)))
-				spew.Dump(val.Field(j))
+				m = common.MapStrUnion(m, getFlatMapStr(val.Field(i).Index(j), fmt.Sprintf("%s-%d", typeOfT.Field(j).Name, j)))
 			}
 		case reflect.Struct:
-			spew.Dump(val.Field(i))
 			for j := 0; j < val.NumField(); j++ {
 				m = common.MapStrUnion(m, getFlatMapStr(val.Field(j), typeOfT.Field(j).Name))
-				spew.Dump(val.Field(j))
 			}
-
 		default:
 			fmt.Println("type switch not found", val.Field(i).Kind())
 
@@ -55,6 +51,5 @@ func eventMapping(stat *uwsgi.Stat) common.MapStr {
 	val := reflect.ValueOf(stat).Elem()
 	event = getFlatMapStr(val, "")
 
-	spew.Dump(event)
 	return event
 }
